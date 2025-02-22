aead468adaeea5e5e29f781d67c749d05a786d56
MarjanDB <mocnik.mario@hotmail.com>
2025-02-22 01:26:04 +0100

    vitest

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,19 +1,24 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app/app.module";
 
-async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+async function prepare(): Promise<NestExpressApplication> {
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	const globalPrefix = "api";
 	app.setGlobalPrefix(globalPrefix);
-	const port = process.env.PORT || 3000;
-	await app.listen(port);
-	Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+	return app;
 }
 
-bootstrap();
+async function bootstrap() {
+	const app = await prepare();
+	const port = process.env.PORT || 3000;
+	await app.listen(port);
+	Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+}
+
+if (import.meta.env.PROD) {
+	bootstrap();
+}
+
+export const viteNodeApp = prepare();
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,7 +30,6 @@ export default [
 		rules: {
 			"@typescript-eslint/interface-name-prefix": "off",
 			"@typescript-eslint/explicit-function-return-type": "off",
-			"@typescript-eslint/explicit-module-boundary-types": "off",
 			"@typescript-eslint/no-explicit-any": "off",
 			"@typescript-eslint/no-namespace": "off",
 			"@nx/enforce-module-boundaries": "error",
