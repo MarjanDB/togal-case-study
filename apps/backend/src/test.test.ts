82e584d070a4e4a32fd243a38d4da1ee9e68cafb
MarjanDB <mocnik.mario@hotmail.com>
2025-03-01 17:57:37 +0100

    jest fixes

--- apps/backend/src/test.test.ts
+++
@@ -1,7 +0,0 @@
-import typia from "typia";
-
-describe("Typia test", () => {
-	it("should work without transformation", () => {
-		expect(typia.assert<{ id: number }>({ id: 123 })).toEqual({ id: 123 });
-	});
-});
